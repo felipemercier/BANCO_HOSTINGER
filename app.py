10b409053faf6679620bd82c85b84d9515870cf2
felipemercier <157895069+felipemercier@users.noreply.github.com>
2025-07-10 16:13:44 -0300

    app.py

--- app.py
+++ app.py
@@ -44,13 +44,14 @@ def inserir_producao():
     tamanho = dados.get("tamanho")
     erp_id = dados.get("erp_id")
     status = dados.get("status")
+    quantidade = dados.get("quantidade", 1)  # valor padrão se não vier do frontend
 
     try:
         conn = pool.get_connection()
         cursor = conn.cursor()
         cursor.execute(
-            "INSERT INTO producao (produto, tamanho, erp_id, status) VALUES (%s, %s, %s, %s)",
-            (produto, tamanho, erp_id, status)
+            "INSERT INTO producao (produto, tamanho, erp_id, status, quantidade) VALUES (%s, %s, %s, %s, %s)",
+            (produto, tamanho, erp_id, status, quantidade)
         )
         conn.commit()
         return jsonify({"mensagem": "Produção inserida com sucesso!"})
